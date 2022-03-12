05a4eafbf23d7b92b95d9058914cc43cf318a653
Tahmina6023 <tahminatamanna799@gmail.com>
2022-03-12 18:02:28 +0600

    Two rwmaining

--- js/myscript.js
+++ js/myscript.js
@@ -4,4 +4,21 @@ $(document).ready(function(){
         meanScreenWidth:'780',
     });
     
+        $(".owl-carousel").owlCarousel({
+            margin:0,
+            items:6,
+            autoplay:true,
+            loop:true,
+            responsive:{
+                0:{
+                    items:2,
+                },
+                600:{
+                    items:4,
+                },
+                1000:{
+                    items:6,
+                }
+            }
+        });
   });
